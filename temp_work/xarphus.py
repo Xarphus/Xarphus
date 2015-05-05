624e704b26fce7fb1f6266ffd1994d52e4289706
Xarphus <dophus@gmail.com>
2015-05-06 00:23:44 +0200

    Update xarphus.py

--- temp_work/xarphus.py
+++ temp_work/xarphus.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
+
+FILE_NAME = "xarphus.py"
 try:
     import sys
     import os
-    from files.modStart import start_mdi
     print FILE_NAME + ": all modules are imported"
 except IOError as (errno, strerror):
     print FILE_NAME + ": I/O error({0}): {1}".format(errno, strerror)
@@ -11,13 +12,10 @@ except:
     print FILE_NAME + ": Unexpected error:", sys.exc_info()[0]
     raise
 
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
 def main():
-        from files.modules_ui.ui_pp_mdi import Mdi_Main
-        window = Mdi_Main()
-        window.show_and_raise()
+    from src.ui_pp_mdi import MDI_Window
+    window = MDI_Window()
+    window.show_and_raise()
 
 if __name__ == "__main__":
     main()
