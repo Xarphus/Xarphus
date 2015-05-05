fb652272d96390fcb9e7e4339638d9f453792bbd
Xarphus <dophus@gmail.com>
2015-05-05 23:45:57 +0200

    Update and rename example_download1.py to downloader.py

--- temp_work/src/downloader.py
+++ temp_work/src/downloader.py
@@ -2,9 +2,6 @@ FILE_NAME = "del_after.py"
 import os
 import requests
 import sys
-import time
-
-
 
 try:
     from PyQt4.QtCore import QThread, pyqtSignal, Qt, QSemaphore
@@ -17,59 +14,25 @@ except ImportError:
 
     print "STATUS [OK]  ", FILE_NAME, ": All modules are imported from PySide"
 
-
-MESSAGEBOX = """
-QMessageBox {
-    background: #222;
-}
-QMessageBox * {
-    background: #222;
-    color: #cccdcc;
-}
-QMessageBox QPushButton {
-    width: 50px;
-    height: 20px;
-    background: #444;
-    color: #aaa;
-}
-"""
-DEFAULT_STYLE = """
-QProgressBar{
-    border: 1px solid grey;
-    background-color: #E6E6E6;
-    border-radius: 5px;
-    text-align: center
-}
-QProgressBar::chunk {
-    background-color: #498EEE;
-    width: 10px;
-    margin: 0px;
-}"""
-
 class Download_Thread(QThread):
     finished_thread = pyqtSignal()
     error_http = pyqtSignal()
     finished_download = pyqtSignal()
     notify_progress = pyqtSignal(int)
 
-    def __init__(self, loc, link, parent=None):
+    def __init__(self, location, link, parent=None):
         QThread.__init__(self, parent)
 
         self.url = link
-        self.location = loc
+        self.location = location
 
         self._run_semaphore = QSemaphore(1)
 
     def run(self):
-        print "log"
         try:
-            time.sleep(0.5)
-            print "Time is over"
             file = requests.get(self.url, stream=True)
             status = file.status_code
-            re = file.reason
-            print "re ", re
-            print status
+
             if not status == 200:
                 self.error_http.emit()
 
@@ -92,7 +55,7 @@ class Download_Thread(QThread):
             with open(self.location, 'wb') as fd:
                 for chunk in file.iter_content(chunk_size):
                     fd.write(chunk)
-                    downloaded_bytes = fd.tell() # sehr genau und direkt
+                    downloaded_bytes = fd.tell()
                     print (float(downloaded_bytes)/file_size*100)
                     self.notify_progress.emit(float(downloaded_bytes)/file_size*100)
 
@@ -117,8 +80,6 @@ class MyCustomDialog(QDialog):
         # Create a progress bar and a button and add them to the main layout
         self.progressBarUpdate = QProgressBar(self)
         self.progressBarUpdate.setAlignment(Qt.AlignCenter)
-        #self.progressBarUpdate.setValue(0)
-        #self.progressBarUpdate.setRange(0, 1)
         layout.addWidget(self.progressBarUpdate)
 
         pushButtonUpdate = QPushButton("Start", self)
@@ -138,19 +99,17 @@ class MyCustomDialog(QDialog):
         self.download_task.error_http.connect(self.on_HTTPError)
         self.download_task.finished_download.connect(self.on_finish_download)
 
-        pushButtonCancel.clicked.connect(self.download_task.stop)
-        self.set_progressbar()
+        pushButtonCancel.clicked.connect(self.on_finished)
 
     def on_start(self):
         self.progressBarUpdate.setRange(0, 0)
         self.download_task.start()
 
     def on_finish_download(self):
-        msh_box = QMessageBox()
-        
-        msh_box.setStyleSheet(MESSAGEBOX)
-        QMessageBox.question(msh_box, ' Message ',
-                                           "The file has been fully downloaded.", msh_box.Ok)
+        msg_box = QMessageBox()
+
+        QMessageBox.question(msg_box, ' Message ',
+                                           "The file has been fully downloaded.", msg_box.Ok)
             
     def on_HTTPError(self):
         reply = QMessageBox.question(self, ' Error ',
@@ -163,11 +122,8 @@ class MyCustomDialog(QDialog):
             event.ignore() 
  
     def on_progress(self, i):
-        self.progressBarUpdate.setRange(0,100)
+        self.progressBarUpdate.setRange(0, 100)
         self.progressBarUpdate.setValue(i)
-
-    def set_progressbar(self):
-        self.progressBarUpdate.setStyleSheet(DEFAULT_STYLE)
  
     def check_folder_exists(self):
         location = os.path.abspath(os.path.join('temp'))
@@ -180,8 +136,8 @@ class MyCustomDialog(QDialog):
             self.on_start()
  
     def on_finished(self):
-        print "Close"
         self.progressBarUpdate.setValue(0)
+        self.close()
 
     def closeEvent(self, event):
         self.download_task.stop()
@@ -190,7 +146,7 @@ def main():
     app = QApplication(sys.argv)
     window = MyCustomDialog()
     window.resize(600, 400)
-    window.show
+    window.show()
     sys.exit(app.exec_())
  
 if __name__ == "__main__":
