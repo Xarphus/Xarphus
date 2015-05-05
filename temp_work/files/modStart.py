af183fd42a43fa89a8b77f2995f3f431d0f07845
Xarphus <dophus@gmail.com>
2015-05-05 23:52:02 +0200

    Delete modStart.py

--- temp_work/files/modStart.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
-def start_mdi():
-        from files.modules_ui.ui_pp_mdi import Mdi_Main
-        window = Mdi_Main()
-        window.show_and_raise()
